d4a4a311ef6eea88f374b37ddbc6b2f5830c92e5
unknown <bryantantzeyong@gmail.com>
2023-06-03 19:16:47 -0500

    simple hello world

---
+++ Hello.cbl
@@ -0,0 +1,13 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM HELLOWORLD.
